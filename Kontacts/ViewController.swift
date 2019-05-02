6b50d3fa473b0f27c2d210764678eba4197557cb
Kapil Rathore <krkapilrathore@gmail.com>
2019-05-02 12:08:11 +0530

    Initial Commit

---
+++ Kontacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kontacts
+//
+//  Created by Kapil Rathore on 02/05/19.
+//  Copyright © 2019 me.kapilrathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
