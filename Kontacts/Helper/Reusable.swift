839e8288df9281a588a432a2655337f138d98a45
Kapil Rathore <krkapilrathore@gmail.com>
2019-05-02 12:28:21 +0530

    Reusable protocol to get default reuse identifier.

---
+++ Kontacts/Helper/Reusable.swift
@@ -0,0 +1,19 @@
+//
+//  Reusable.swift
+//  Kontacts
+//
+//  Created by Kapil Rathore on 02/05/19.
+//  Copyright © 2019 me.kapilrathore. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+  static var reuseId: String { get }
+}
+
+extension ReusableView where Self: UIView {
+  static var reuseId: String {
+    return String(describing: self)
+  }
+}
